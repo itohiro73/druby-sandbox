00b255e15d07f2e20237053edaec90cf8b60f13b
Hiroshi Ito <itohiro73@gmail.com>
2020-09-09 00:03:48 +0900

    Rename stub => local_io.

--- chat_server.rb
+++ chat_server.rb
@@ -1,8 +1,8 @@
 require 'drb'
 
 class ChatService
-  def initialize(stub)
-    @local_stub = stub
+  def initialize(local_io)
+    @local_io = local_io
     @users = Hash.new
   end
 
@@ -16,17 +16,17 @@ class ChatService
   end
 
   def chat(uri, comment)
-    @local_stub.chat(@users[uri], comment)
+    @local_io.chat(@users[uri], comment)
   end
 end
 
-class Stub
+class LocalIO
   def chat(name, comment)
     puts name + ": " + comment
   end
 end
 
-stub = Stub.new
-service = ChatService.new(stub)
+io = LocalIO.new
+service = ChatService.new(io)
 DRb.start_service('druby://localhost:54300', service)
 sleep
